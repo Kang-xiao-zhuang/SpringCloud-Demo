ca795a19f6cd291d460df3c9f93090bcc6b652ca
Kang-xiao-zhuang <2247830091@qq.com>
2023-03-25 00:06:07 +0800

    更换服务器地址

--- es-demo/src/main/java/com/zhuang/es/HotelDemoApplication.java
+++ es-demo/src/main/java/com/zhuang/es/HotelDemoApplication.java
@@ -17,9 +17,9 @@ public class HotelDemoApplication {
     }
 
     @Bean
-    public RestHighLevelClient restHighLevelClient(){
+    public RestHighLevelClient restHighLevelClient() {
         return new RestHighLevelClient(RestClient.builder(
-                HttpHost.create("http://192.168.150.101:9200")
+                HttpHost.create("http://192.168.18.128:9200")
         ));
     }
 }
