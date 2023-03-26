7875a88ae83243c2c87052a44b27fe43d12c5618
Kang-xiao-zhuang <2247830091@qq.com>
2023-03-26 21:05:09 +0800

    es聚合查询测试

--- es-demo/src/test/java/com/zhuang/es/HotelSearchTest.java
+++ es-demo/src/test/java/com/zhuang/es/HotelSearchTest.java
@@ -12,9 +12,15 @@ import org.elasticsearch.index.query.BoolQueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.search.SearchHit;
 import org.elasticsearch.search.SearchHits;
+import org.elasticsearch.search.aggregations.*;
+import org.elasticsearch.search.aggregations.bucket.terms.Terms;
 import org.elasticsearch.search.fetch.subphase.highlight.HighlightBuilder;
 import org.elasticsearch.search.fetch.subphase.highlight.HighlightField;
 import org.elasticsearch.search.sort.SortOrder;
+import org.elasticsearch.search.suggest.Suggest;
+import org.elasticsearch.search.suggest.SuggestBuilder;
+import org.elasticsearch.search.suggest.SuggestBuilders;
+import org.elasticsearch.search.suggest.completion.CompletionSuggestion;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -22,7 +28,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.util.CollectionUtils;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.Map;
+import java.util.concurrent.CompletionException;
 
 /**
  * description: HotelSearchTest
@@ -177,5 +185,59 @@ public class HotelSearchTest {
             }
             System.out.println("hotelDoc = " + hotelDoc);
         }
+    }
+
+    @Test
+    void testAggreation() throws IOException {
+        // 解析响应
+        SearchRequest request = new SearchRequest("hotel");
+        // 准备DSL设置size
+        request.source().size(0);
+        // 聚合
+        request.source().aggregation(AggregationBuilders
+                .terms("brandAgg")
+                .field("brand")
+                .size(10));
+        // 发出请求
+        SearchResponse response = client.search(request, RequestOptions.DEFAULT);
+
+        // 解析聚合结果
+        Aggregations aggregations = response.getAggregations();
+        // 根据名称获取聚合结果
+        Terms brandTerms = aggregations.get("brand_agg");
+        // 获取桶
+        List<? extends Terms.Bucket> buckets = brandTerms.getBuckets();
+
+        for (Terms.Bucket bucket : buckets) {
+            String brandName = bucket.getKeyAsString();
+            System.out.println("brandName = " + brandName);
+        }
+        System.out.println(response);
+    }
+
+    @Test
+    void testSuggest() throws IOException {
+        SearchRequest request = new SearchRequest("hotel");
+
+        request.source().suggest(new SuggestBuilder().addSuggestion("suggestions",
+                SuggestBuilders.completionSuggestion("suggestion")
+                        .prefix("h").skipDuplicates(true).size(10)));
+
+        SearchResponse response = client.search(request, RequestOptions.DEFAULT);
+        System.out.println("response = " + response);
+
+        // 解析结果
+        Suggest suggest = response.getSuggest();
+        // 根据补全查询名称，获取补全结果
+        CompletionSuggestion suggestions = suggest.getSuggestion("suggestions");
+        // 获取options
+        List<CompletionSuggestion.Entry.Option> options = suggestions.getOptions();
+        // 遍历
+        for (CompletionSuggestion.Entry.Option option : options) {
+            String text = option.getText().toString();
+            System.out.println("text = " + text);
+        }
+
+
     }
 }
