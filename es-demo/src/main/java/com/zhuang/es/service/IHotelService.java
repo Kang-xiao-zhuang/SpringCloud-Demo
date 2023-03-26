e54ea130dbb03530c1fc2ad56895d6447c4ea48b
Kang-xiao-zhuang <2247830091@qq.com>
2023-03-26 21:08:21 +0800

    新增过滤自动补全方法

--- es-demo/src/main/java/com/zhuang/es/service/IHotelService.java
+++ es-demo/src/main/java/com/zhuang/es/service/IHotelService.java
@@ -6,6 +6,13 @@ import com.zhuang.es.pojo.Hotel;
 import com.zhuang.es.pojo.PageResult;
 import com.zhuang.es.pojo.RequestParams;
 
+import java.util.List;
+import java.util.Map;
+
 public interface IHotelService extends IService<Hotel> {
     PageResult search(RequestParams params);
+
+    Map<String, List<String>> filters(RequestParams params);
+
+    List<String> getSuggestions(String prefix);
 }
--- es-demo/src/main/java/com/zhuang/es/service/impl/HotelService.java
+++ es-demo/src/main/java/com/zhuang/es/service/impl/HotelService.java
@@ -22,14 +22,22 @@ import org.elasticsearch.index.query.functionscore.FunctionScoreQueryBuilder;
 import org.elasticsearch.index.query.functionscore.ScoreFunctionBuilders;
 import org.elasticsearch.search.SearchHit;
 import org.elasticsearch.search.SearchHits;
+import org.elasticsearch.search.aggregations.AggregationBuilders;
+import org.elasticsearch.search.aggregations.Aggregations;
+import org.elasticsearch.search.aggregations.bucket.terms.Terms;
 import org.elasticsearch.search.fetch.subphase.highlight.HighlightField;
 import org.elasticsearch.search.sort.SortBuilders;
 import org.elasticsearch.search.sort.SortOrder;
+import org.elasticsearch.search.suggest.Suggest;
+import org.elasticsearch.search.suggest.SuggestBuilder;
+import org.elasticsearch.search.suggest.SuggestBuilders;
+import org.elasticsearch.search.suggest.completion.CompletionSuggestion;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -158,5 +166,104 @@ public class HotelService extends ServiceImpl<HotelMapper, Hotel> implements IHo
             hotels.add(hotelDoc);
         }
         return new PageResult(total, hotels);
+    }
+
+    @Override
+    public Map<String, List<String>> filters(RequestParams params) {
+        try {
+            // 1.准备Request
+            SearchRequest request = new SearchRequest("hotel");
+            // 2.准备DSL
+            // 2.1.query
+            buildBasicQuery(params, request);
+            // 2.2.设置size
+            request.source().size(0);
+            // 2.3.聚合
+            buildAggregation(request);
+            // 3.发出请求
+            SearchResponse response = restHighLevelClient.search(request, RequestOptions.DEFAULT);
+            // 4.解析结果
+            Map<String, List<String>> result = new HashMap<>();
+            Aggregations aggregations = response.getAggregations();
+            // 4.1.根据品牌名称，获取品牌结果
+            List<String> brandList = getAggByName(aggregations, "brandAgg");
+            result.put("品牌", brandList);
+            // 4.2.根据品牌名称，获取品牌结果
+            List<String> cityList = getAggByName(aggregations, "cityAgg");
+            result.put("城市", cityList);
+            // 4.3.根据品牌名称，获取品牌结果
+            List<String> starList = getAggByName(aggregations, "starAgg");
+            result.put("星级", starList);
+
+            return result;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void buildAggregation(SearchRequest request) {
+        request.source().aggregation(AggregationBuilders
+                .terms("brandAgg")
+                .field("brand")
+                .size(100)
+        );
+        request.source().aggregation(AggregationBuilders
+                .terms("cityAgg")
+                .field("city")
+                .size(100)
+        );
+        request.source().aggregation(AggregationBuilders
+                .terms("starAgg")
+                .field("starName")
+                .size(100)
+        );
+    }
+
+    private List<String> getAggByName(Aggregations aggregations, String aggName) {
+        // 4.1.根据聚合名称获取聚合结果
+        Terms brandTerms = aggregations.get(aggName);
+        // 4.2.获取buckets
+        List<? extends Terms.Bucket> buckets = brandTerms.getBuckets();
+        // 4.3.遍历
+        List<String> brandList = new ArrayList<>();
+        for (Terms.Bucket bucket : buckets) {
+            // 4.4.获取key
+            String key = bucket.getKeyAsString();
+            brandList.add(key);
+        }
+        return brandList;
+    }
+
+    @Override
+    public List<String> getSuggestions(String prefix) {
+        try {
+            // 1.准备Request
+            SearchRequest request = new SearchRequest("hotel");
+            // 2.准备DSL
+            request.source().suggest(new SuggestBuilder().addSuggestion(
+                    "suggestions",
+                    SuggestBuilders.completionSuggestion("suggestion")
+                            .prefix(prefix)
+                            .skipDuplicates(true)
+                            .size(10)
+            ));
+            // 3.发起请求
+            SearchResponse response = restHighLevelClient.search(request, RequestOptions.DEFAULT);
+            // 4.解析结果
+            Suggest suggest = response.getSuggest();
+            // 4.1.根据补全查询名称，获取补全结果
+            CompletionSuggestion suggestions = suggest.getSuggestion("suggestions");
+            // 4.2.获取options
+            List<CompletionSuggestion.Entry.Option> options = suggestions.getOptions();
+            // 4.3.遍历
+            List<String> list = new ArrayList<>(options.size());
+            for (CompletionSuggestion.Entry.Option option : options) {
+                String text = option.getText().toString();
+                list.add(text);
+            }
+            return list;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
