5b5e04cb6661d5fc2a1244e033eaca5128b62840
Kang-xiao-zhuang <2247830091@qq.com>
2023-04-01 22:41:28 +0800

    链路模式相关代码修改

--- order-service/src/main/java/cn/itcast/order/web/OrderController.java
+++ order-service/src/main/java/cn/itcast/order/web/OrderController.java
@@ -2,12 +2,15 @@ package cn.itcast.order.web;
 
 import cn.itcast.order.pojo.Order;
 import cn.itcast.order.service.OrderService;
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("order")
 public class OrderController {
@@ -15,9 +18,29 @@ public class OrderController {
    @Autowired
    private OrderService orderService;
 
+   @SentinelResource("hot")
     @GetMapping("{orderId}")
     public Order queryOrderByUserId(@PathVariable("orderId") Long orderId) {
         // 根据id查询订单并返回
         return orderService.queryOrderById(orderId);
+    }
+
+    @GetMapping("/query")
+    public String queryOrder(){
+        orderService.queryGoods();
+        log.info("查询订单");
+        return "查询订单成功";
+    }
+
+    @GetMapping("/save")
+    public String saveOrder(){
+        orderService.queryGoods();
+        log.info("新增订单");
+        return "新增订单成功";
+    }
+
+    @GetMapping("/update")
+    public String updateOrder(){
+        return "更新订单成功";
     }
 }
